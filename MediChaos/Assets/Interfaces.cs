20b1a9a64784335d8a23aa6fc9bb2947727d5b2c
DarkFire1012 <tiongwunlim@gmail.com>
2021-06-07 15:40:49 +0800

    Fix 5/6

---
+++ MediChaos/Assets/Interfaces.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interfaces //: MonoBehaviour
+{
+
+
+
+}
+public class ButtonEvents
+{
+    public interface open
+    {
+        
+
+
+
+
+    }
+
+}
